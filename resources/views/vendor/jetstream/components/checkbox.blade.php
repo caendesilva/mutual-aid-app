0f3743ce7fda432f814ca0c3611723e476fd2bce
Caen De Silva <caen@desilva.se>
2022-03-07 21:45:00 +0100

    Update form styling

--- resources/views/vendor/jetstream/components/checkbox.blade.php
+++ resources/views/vendor/jetstream/components/checkbox.blade.php
@@ -1,1 +1,1 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-600 text-indigo-600 shadow-sm focus:border-indigo-600 focus:ring focus:ring-indigo-500 focus:ring-opacity-75']) !!}>
+<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-600 text-indigo-600 shadow-sm focus:border-indigo-600 focus:ring focus:ring-indigo-500 focus:ring-opacity-75 cursor-pointer']) !!}>
--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-600 focus:border-indigo-600 focus:ring focus:ring-indigo-500 focus:ring-opacity-75 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-400 focus:border-indigo-600 focus:ring focus:ring-indigo-500 focus:ring-opacity-75 rounded-md shadow-sm']) !!}>
