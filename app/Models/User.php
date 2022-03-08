ed091a4cf3360daa222203e596bfcb9092f8c225
Caen De Silva <caen@desilva.se>
2022-03-08 13:22:44 +0100

    Add an attribute to get a list of the user's roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Core\Roles;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -103,5 +104,17 @@ class User extends Authenticatable
     public function roles(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(Role::class);
+    }
+
+    /**
+     * Get the user's roles as a comma separated list.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function roleList(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => implode(', ', $this->roles()->pluck('name')->toArray()),
+        );
     }
 }
