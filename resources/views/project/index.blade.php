7f079d926234608339925c2eac1dc001a5b8f58b
Caen De Silva <caen@desilva.se>
2022-03-08 19:49:24 +0100

    Move the post card to component

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -16,29 +16,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="">
                 @if($models->count())
-                    @foreach ($models as $model)
-                        <article class="m-4 my-6 p-4 lg:px-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                            <header>
-                                <h3 class="text-lg font-bold">
-                                    <a href="{{ route($modelName. 's.show', $model) }}">{{ $model->subject }}</a>
-                                </h3>
-                                {{ ucwords($modelName) }}ed <time datetime="{{ $model->created_at }} {{ $model->created_at->format('e') }}" title="{{ $model->created_at }} {{ $model->created_at->format('e') }}">{{ $model->niceDate }}</time>
-                                by <address class="inline" rel="author" style="display: inline;">{{ $model->user->name }}</address>.
-                            </header>
-                            <div>
-                                @if($model->resources)
-                                <strong>Needs:</strong>
-                                {{ ucwords(implode(', ', $model->resources)) }}
-                                @endif
-                            </div>
-                            <footer class="mt-3">
-                                <a class="text-indigo-700" href="{{ route($modelName. 's.show', $model) }}">View {{ ucwords($modelName) }}</a>
-                                @can('update', $model)
-                                <a class="text-indigo-700 mx-2 text-sm" href="{{ route($modelName. 's.edit', $model) }}">Edit {{ ucwords($modelName) }}</a>
-                                @endcan
-                            </footer>
-                        </article>
-                    @endforeach
+                    <section>
+                        @foreach ($models as $model)
+                            @include('project.index-card')
+                        @endforeach
+                    </section>
 
                     <footer class="m-4">
                         {{ $models->links() }}
