a0a5f3e2545094a0afb50a3f1491d389f7d5834b
Caen De Silva <caen@desilva.se>
2022-03-09 20:07:57 +0100

    Create the base class

---
+++ app/Http/Controllers/DistanceController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Calculate the distance between two points
+ * Requires the points to be sets of coordinates,
+ * and includes a helper to convert a ZIP code to GPS.
+ */
+class DistanceController extends Controller
+{
+    /**
+     * The Earth radius used for calculations.
+     * Based on the WGS 84 datum surface which has
+     * an equatorial radius (a) of 6 378 137 meters 
+     */
+    public const EARTH_RADIUS = 6378.137;
+
+    /**
+     * The point A latitude
+     * @var float φ1
+     */
+    public float $lat1 = 59.3293;
+
+    /**
+     * The point A longitude
+     * @var float λ1
+     */
+    public float $long1 = 18.0686;
+
+    /**
+     * The point B latitude
+     * @var float φ2
+     */
+    public float $lat2 = 51.5072;
+
+    /**
+     * The point B longitude
+     * @var float λ2
+     */
+    public float $long2 = 0.1276;
+
+    /**
+     * Coordinate array of point A 
+     * @var array [φ1, λ1]
+     */
+    public array $tup1;
+
+    /**
+     * Coordinate array of point B 
+     * @var array [φ2, λ2]
+     */
+    public array $tup2;
+
+    /**
+     * The calculated distance in meters.
+     * @var float
+     */
+    public float $distance;
+
+    /**
+     * An array of the calculated distance in varying formats.
+     * @var array
+     */
+    public array $results;
+
+    /**
+     * Construct the class.
+     */
+    public function __construct() {
+        $this->tup1 = [$this->lat1, $this->long1];
+        $this->tup2 = [$this->lat2, $this->long2];
+
+        $this->distance = $this->calculate();
+
+        $this->results = [
+            'kilometers' => $this->distance / 1000,
+            'kilometersRounded' => round($this->distance / 1000),
+            'miles' => ($this->distance * 0.00062137),
+            'milesRounded' => round($this->distance * 0.00062137),
+        ];
+    }
+
+    /**
+     * Calculate the distance between two GCS tuples
+     * 
+     * The base formula is from Amy (user v-xicai) at Microsoft BI and is as follows
+     * =acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lon2-lon1))*6371 (where 6371 is Earth radius in km.)
+     * 
+     * And thanks to https://stackoverflow.com/a/49212829/5700388 (with the great quote "education never seems like a waste of time")
+     * we know that distance = earth radius * radians.
+     * 
+     * Note that the coordinate trigonometric arguments are in degrees, but the arccosine result
+     * in the formula returns radians which we multiply with the Earth radius to get the distance.
+     */
+    public function calculate(): float
+    {
+        // Convert the degree values to radians
+        $lat1 = deg2rad($this->lat1);
+        $lat2 = deg2rad($this->lat2);
+        $long1 = deg2rad($this->long1);
+        $long2 = deg2rad($this->long2);
+
+        // Return the result of the formula in meters
+        return acos(sin($lat1)*sin($lat2)+cos($lat1)*cos($lat2)*cos($long2-$long1))*self::EARTH_RADIUS * 1000;
+    }
+}
