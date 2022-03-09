f0b79bca0bb691d4898332907380ca6db01cff95
Caen De Silva <caen@desilva.se>
2022-03-09 13:50:15 +0100

    Move the religious provider badge into a component

--- resources/views/project/index-card.blade.php
+++ resources/views/project/index-card.blade.php
@@ -1,11 +1,7 @@
 <article class="m-4 my-6 p-4 lg:px-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
 	<header>
 		@if($modelName === 'offer' && $model->is_religious)
-		<div>
-			<span class="sr-only">This offer is from a religious organisation.
-				You can filter out these results on your <a href="{{ route('profile.show') }}">Account Page</a>.</span>
-			<span class="float-right" title="Religious Provider" role="presentation" aria-hidden="true">⛪</span>
-		</div>
+		<x-badges-religious-provider class="float-right" />
 		@endif
 		<h3 class="text-lg font-bold">
 			<a href="{{ route($modelName. 's.show', $model) }}">{{ $model->subject }}</a>
