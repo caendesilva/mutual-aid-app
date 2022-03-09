10f456413abd9371f74c3f8cf5f656d9777811d9
Caen De Silva <caen@desilva.se>
2022-03-09 14:37:16 +0100

    Fix grid bug causing section to collapse on mobile

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -83,7 +83,7 @@
         </div>
     </div>
 
-    <div class="p-6 border-t border-gray-200 col-span-2">
+    <div class="p-6 border-t border-gray-200 md:col-span-2">
         <div class="flex items-center">
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
             <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Notices</div>
