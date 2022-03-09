2f64a67cb8a6eaba6dd189711218fe5d5516a1b7
Caen De Silva <caen@desilva.se>
2022-03-09 14:05:10 +0100

    Add class support

--- resources/views/components/badges-religious-provider.blade.php
+++ resources/views/components/badges-religious-provider.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes }}>
+<div {{ $attributes ?? '' }}>
     <span class="sr-only">This offer is from a religious organisation.
         You can filter out these results on your <a href="{{ route('profile.show') }}">Account Page</a>.</span>
     <span title="Religious Provider" role="presentation" aria-hidden="true">⛪</span>
