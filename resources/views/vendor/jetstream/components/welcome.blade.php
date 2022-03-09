3d2427b1d950022a54315ea8beef645b44e99e1a
Caen De Silva <caen@desilva.se>
2022-03-09 11:15:51 +0100

    Update notices section

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -83,29 +83,17 @@
         </div>
     </div>
 
-    <div class="p-6 border-t border-gray-200">
+    <div class="p-6 border-t border-gray-200 col-span-2">
         <div class="flex items-center">
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
             <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Notices</div>
         </div>
         <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500 prose prose">
-                {!! substr((new App\Http\Controllers\NoticeController())->__invoke(), 0, 320) !!}
+            <div class="mt-2 text-sm text-gray-500 prose max-w-2xl">
+                {!! ((new App\Http\Controllers\NoticeController())->__invoke()) !!}
             </div>
             <a href="/notices.txt" class="text-sm text-indigo-500 ml-7">Full list of notices</a>
         </div>
     </div>
 
-    <div class="p-6 border-t border-gray-200 md:border-l">
-        <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path></svg>
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">Authentication</div>
-        </div>
-
-        <div class="ml-12">
-            <div class="mt-2 text-sm text-gray-500">
-                Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started what matters most: building your application.
-            </div>
-        </div>
-    </div>
 </div>
