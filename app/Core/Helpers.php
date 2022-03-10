486b8e0c6d9fadb105f8b5c237ee9c1fbcf63a34
Caen De Silva <caen@desilva.se>
2022-03-10 21:35:28 +0100

    Create Helpers.php

---
+++ app/Core/Helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!function_exists('chance')) {
+    /**
+     * Return true for $probability percent of the time.
+     * @example chance(25) returns true 25% of the time.
+     *
+     * Has a ~0.003527512605377% mean deviation when running 10 000 000 iterations.
+     *
+     * @param int $probability factor in percent expressed as an integer between 0 and 99
+     * @return bool
+     */
+    function chance(int $probability = 50): bool {
+        return mt_rand(0, 99) < $probability;
+    }
+}
