e52d654225c56b01f5806d1019b231ff3150fea0
Caen De Silva <caen@desilva.se>
2022-03-11 19:40:37 +0100

    Improve support for auxiliary buttons

--- resources/views/components/forms/actions.blade.php
+++ resources/views/components/forms/actions.blade.php
@@ -1,13 +1,16 @@
-<div class="flex flex-row -mx-2 mt-5 justify-end">
-    @if($hasDeleteButton ?? false)
-    <x-jet-danger-button type="button" class="m-2 mr-auto" wire:click="$toggle('confirmingModelDeletion')" wire:loading.attr="disabled">
-        Delete
-    </x-jet-danger-button>
+<div class="flex flex-row flex-wrap -mx-2 mt-5 justify-start sm:justify-end">
+    @if($auxiliaryButtons ?? false)
+    <div class="mr-3 sm:mr-auto sm:ml-0">
+        {{ $auxiliaryButtons }}
+    </div>
     @endif
-    <x-jet-secondary-button type="reset" class="m-2">
-        Reset
-    </x-jet-secondary-button>
-    <x-jet-button type="submit" class="m-2">
-        Submit
-    </x-jet-button>
+
+    <div>
+        <x-jet-secondary-button type="reset" class="m-2">
+            Reset
+        </x-jet-secondary-button>
+        <x-jet-button type="submit" class="m-2">
+            Submit
+        </x-jet-button>
+    </div>
 </div>
