bb0ee9898b48db1ff3eef565e75018ee59895f32
Caen De Silva <caen@desilva.se>
2022-03-11 15:23:08 +0100

    Create reusable component with form actions

---
+++ resources/views/components/forms/actions.blade.php
@@ -0,0 +1,8 @@
+<div class="flex flex-row -mx-1 mt-5 justify-end">
+    <x-jet-secondary-button type="reset" class="m-2">
+        Reset
+    </x-jet-secondary-button>
+    <x-jet-button type="submit" class="m-2">
+        Submit
+    </x-jet-button>
+</div>
