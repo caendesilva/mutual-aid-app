722a6ef9f705e2d7b68e77093cfa73f310dbfd9a
Caen De Silva <caen@desilva.se>
2022-03-11 17:25:39 +0100

    Add dropdown menu

--- resources/views/listing/show.blade.php
+++ resources/views/listing/show.blade.php
@@ -5,11 +5,54 @@
                 {{ __("$Type Details") }}
             </h2>
             @can('update', $listing)
-            <a href="{{ route('listings.edit', $listing) }}">
-                <x-jet-button>
-                	{{ __("Update $Type") }}
-                </x-jet-button>
-            </a>
+            <div class="ml-3 relative">
+                <x-jet-dropdown align="right">
+                    <x-slot name="trigger">
+                        <span class="inline-flex rounded-md">
+                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
+                                <x-jet-button>
+                                    {{ __("Manage $Type") }}
+                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </x-jet-button>
+                            </button>
+                        </span>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <div>
+                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                {{ __("Manage $Type") }}
+                            </div>
+                            
+                            <x-jet-dropdown-link href="{{ route('listings.edit', $listing) }}">
+                                <div class="inline-flex">
+                                    {{ __("Update $Type") }}
+                                    <!-- Icons by Google Material Icons (License Apache 2.0) -->
+                                    <svg class="fill-indigo-500 ml-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
+                                </div>    
+                            </x-jet-dropdown-link>
+
+                            <x-jet-dropdown-link href="{{ route('listings.destroy', $listing) }}">
+                                <div class="inline-flex">
+                                    {{ __("Delete $Type") }}
+                                    <!-- Icons by Google Material Icons (License Apache 2.0) -->
+                                    <svg class="fill-red-500 ml-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
+                                </div>    
+                            </x-jet-dropdown-link>
+
+                            <x-jet-dropdown-link href="{{ route('listings.edit', $listing) }}">
+                                <div class="inline-flex">
+                                    {{ __("Mark as Solved") }}
+                                    <!-- Icons by Google Material Icons (License Apache 2.0) -->
+                                    <svg class="fill-green-500 ml-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
+                                </div>    
+                            </x-jet-dropdown-link>
+                        </div>
+                    </x-slot>
+                </x-jet-dropdown>
+            </div>
             @endcan
         </div>
     </x-slot>
