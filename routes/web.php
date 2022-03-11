5eda63d89959fd916f04e8168a1b27ba1f7e92d0
Caen De Silva <caen@desilva.se>
2022-03-11 21:02:59 +0100

    Remove the deprecated models

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\NoticeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OfferController;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\RequestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-// Route::resource('projects', ProjectController::class);
-Route::resource('requests', RequestController::class);
-Route::resource('offers', OfferController::class);
-
 Route::resource('listings', ListingController::class);
-
 
 Route::middleware(['auth:sanctum', 'verified', 'can:accessDashboard'])->get('/dashboard', function () {
     return view('dashboard');
