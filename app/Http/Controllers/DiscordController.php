daf70d408fad41b3eab811d63b63b79934c6a2c6
Caen De Silva <caen@desilva.se>
2022-04-14 12:19:18 +0200

    Create the base Discord integration

---
+++ app/Http/Controllers/DiscordController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DiscordEvent;
+use App\Notifications\DiscordNotification;
+use Illuminate\Http\Request;
+
+class DiscordController extends Controller
+{
+    public function ping()
+    {
+        return (new DiscordEvent)->notify(new DiscordNotification);
+    }
+}
---
+++ app/Notifications/DiscordNotification.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Discord\DiscordChannel;
+use NotificationChannels\Discord\DiscordMessage;
+
+class DiscordNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [DiscordChannel::class];
+    }
+
+    public function toDiscord($notifiable)
+    {
+        return DiscordMessage::create("Beep Boop I'm a bot! 🤖");
+    }
+}
