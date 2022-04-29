6f9768a9e22d8d4750eb426565730e9d3a676d0c
Caen De Silva <caen@desilva.se>
2022-04-29 22:25:26 +0200

    Add policy links to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,4 +5,17 @@
     <small>
     {{ $version }}        
     </small>
+    <div>
+        <ul class="flex flex-wrap mx-auto justify-center text-center text-sm mt-3 mb-2">
+            <li>
+                <a href="{{ route('guidelines.show') }}" class="text-gray-500 hover:text-gray-900 m-2">Community Guidelines</a>
+            </li>
+            <li>
+                <a href="{{ route('terms.show') }}" class="text-gray-500 hover:text-gray-900 m-2">Terms of Service</a>
+            </li>
+            <li>
+                <a href="{{ route('policy.show') }}" class="text-gray-500 hover:text-gray-900 m-2">Privacy Policy</a>
+            </li>
+        </ul>
+    </div>
 </footer>
